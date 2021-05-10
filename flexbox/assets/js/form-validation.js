191674d6ac554b78b18acf1363f7a5825ce7ce7b
deigo-lps <diego.lopes0909@gmail.com>
2021-05-10 15:21:38 -0300

    ajeitando regex

--- flexbox/assets/js/form-validation.js
+++ flexbox/assets/js/form-validation.js
@@ -1,13 +1,16 @@
-const form=document.getElementsByTagName("form")[0];
-const input=form.getElementsByTagName("input");
+const form=document.getElementById("get_started_form");
+const name=document.getElementById("input_name");
+const phone=document.getElementById("input_number");
+const email=document.getElementById("input_email");
+const zip=document.getElementById("input_zip");
 
-const regPhone=/^[(][0-9]{2}[)][0-9]{4}[-][0-9]{4}$/;
+const regPhone=/^[(][0-9]{2}[)]([0-9]{4}|[0-9]{5})[-][0-9]{4}$/;
 const regEmail=/^([a-z0-9.-_]+)([@])([a-z]+)([.])([a-z]+)$/;
 const regZip=/^[0-9]{2}[.][0-9]{3}[-][0-9]{3}$/;
 form.addEventListener("submit",function(){
   event.preventDefault();
-  if(regPhone.test(input[1].value)&&regEmail.test(input[2].value)&&regZip.test(input[3].value))
-    alert("Obrigado por se cadastrar, "+input[0].value+"!");
+  if(regPhone.test(phone.value)&&regEmail.test(email.value)&&regZip.test(zip.value))
+    alert("Obrigado por se cadastrar, "+name.value+"!");
   else {
     alert("Preencha os campos corretamente.")
   }
